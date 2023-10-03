53c10cb35333f02ad77655e5d3f39fbafb1e81d1
unknown <adrianfernadezrodriguez@alumnos.tuniversformacion.es>
2023-10-03 10:35:44 +0200

    Hacemos la prueba no evaluable

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const num = prompt();
+
+if(num>0){
+    console.log("El numero es positivo")
+    alert("El numero es positivo")
+}else if(num<0){
+    console.log("El numero es negativo")
+    alert("El numero es negativo")
+}else{
+    console.log("El numero es 0")
+    alert("El numero es 0")
+}
